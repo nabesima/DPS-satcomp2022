29e229dd60adb72434d5e487e0daa6f8e97679b8
NABESHIMA Hidetomo <nabesima@yamanashi.ac.jp>
2022-05-22 21:22:43 +0900

    set margin to 0

--- leader/run-dps.sh
+++ leader/run-dps.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-/DPS-1.4.0-sc2022 --nthreads=32 --mem-lim=262144 --verbose=0 --model --ks-pakis --non-det $1
+/DPS-1.4.0-sc2022 --nthreads=32 --mem-lim=262144 --verbose=0 --model --ks-pakis -m 0 --non-det $1
